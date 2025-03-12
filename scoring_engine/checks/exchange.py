54cbe6bff64a1848559886b318256eb5df8fe7b2
voraci0us <harrisonrkeating@gmail.com>
2025-03-12 12:04:18 -0400

    Adds authenticated check for Exchange OWA

---
+++ scoring_engine/checks/exchange.py
@@ -0,0 +1,15 @@
+from scoring_engine.engine.basic_check import BasicCheck, CHECKS_BIN_PATH
+
+
+class ExchangeCheck(BasicCheck):
+    CMD = CHECKS_BIN_PATH + '/exchange_check {0} {1} {2} {3}'
+
+    def command_format(self, properties):
+        account = self.get_random_account()
+        return (
+            self.host,
+            self.port,
+            account.username,
+            account.password
+        )
+
